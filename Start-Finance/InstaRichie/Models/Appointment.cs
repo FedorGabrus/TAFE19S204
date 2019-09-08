9c3e6e25499ee79c94951e996406a2d193c93d7e
Chris Taylor <christopher.taylor8@student.tafesa.edu.au>
2019-09-08 15:50:54 +0930

    added the required functionality (add and delete appointments)

---
+++ Start-Finance/InstaRichie/Models/Appointment.cs
@@ -0,0 +1,25 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models {
+
+   public class Appointment{
+
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [NotNull]
+        public string Name { get; set; }
+
+        [NotNull]
+        public string DateOfAppt { get; set; }
+
+        [NotNull]
+        public string TimeOfAppt { get; set; }
+
+    }
+}
